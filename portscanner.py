6e654f7e12cd94674c832113164e8121339e40cb
Robert Monden <dev@robertmonden.com>
2018-03-09 08:16:14 +0100

    Synching repository

--- portscanner.py
+++ portscanner.py
@@ -4,115 +4,115 @@ import sys
 import os
 import socket
 
-portscan_version = '1.0.0'
-portscan_copyright = '2017, Robert Monden'
+PORTSCAN_VERSION = '1.0.1'
+PORTSCAN_COPYRIGHT = '2017, Robert Monden'
 
-port_max = 65535
-port_min = 1
+PORT_MAX = 65535
+PORT_MIN = 1
 
 terminal_width = os.get_terminal_size().columns
 
 
 def init():
-	print ('*** PORT SCANNER ***')
-	print ('Version: ' + portscan_version)
-	print ('Copyright: ' + portscan_copyright)
+    print('*** PORT SCANNER ***')
+    print('Version: ' + PORTSCAN_VERSION)
+    print('Copyright: ' + PORTSCAN_COPYRIGHT)
 
-	arguments = get_args()
+    arguments = get_args()
 
-	if len(arguments) < 3:
-		check_target(arguments[1], all_ports=True)
-	else:
-		ports = parse_ports(arguments[2])
-		check_target(arguments[1], int(ports[0]), int(ports[1]))
+    if len(arguments) < 3:
+        check_target(arguments[1], all_ports=True)
+    else:
+        ports = parse_ports(arguments[2])
+        check_target(arguments[1], int(ports[0]), int(ports[1]))
 
 
-def report(message, hasStatus = False, status = True, dots = True):
-	global terminal_width
+def report(message, hasStatus=False, status=True, dots=True):
+    global terminal_width
 
-	if hasStatus:
-	    print (message + '... ' + str('OK' if status else 'FAILED').rjust(terminal_width - (len(message) + 5)))
-	elif dots:
-	    print (message + '...')
-	else:
-		print (message)
+    if hasStatus:
+        print(message + '... ' + str('OK' if status else 'FAILED').rjust(terminal_width - (len(message) + 5)))
+    elif dots:
+        print(message + '...')
+    else:
+        print(message)
 
 
 def report_error(message):
-	print ('  [error] ' + message)
-	sys.exit(0)
+    print('  [error] ' + message)
+    sys.exit(0)
 
 
 def get_args():
-	if len(sys.argv) < 2:
-		report_error('Not enough parameters')
+    if len(sys.argv) < 2:
+        report_error('Not enough parameters')
 
-	elif len(sys.argv) > 3:
-		report_error('Too many parameters')
+    elif len(sys.argv) > 3:
+        report_error('Too many parameters')
 
-	return sys.argv
+    return sys.argv
 
 
 def parse_ports(ports):
-	port_range = ports.split(':')
+    port_range = ports.split(':')
 
-	if len(port_range) > 2 and len(port_range) < 1:
-		report_error('Invalid port range')
+    if len(port_range) > 2 and len(port_range) < 1:
+        report_error('Invalid port range')
 
-	for portnr in port_range:
-		if not portnr.isdigit():
-			report_error('Ports should be numeric')
+    for portnr in port_range:
+        if not portnr.isdigit():
+            report_error('Ports should be numeric')
 
-		elif int(portnr) > port_max:
-			report_error('Call to nonexisting port. Maximum value: ' + str(port_max))
+        elif int(portnr) > PORT_MAX:
+            report_error('Call to nonexisting port. Maximum value: ' + str(PORT_MAX))
 
-		elif int(portnr) < port_min:
-			report_error('Call to nonexisting port. Minimum value: ' + str(port_min));
+        elif int(portnr) < PORT_MIN:
+            report_error('Call to nonexisting port. Minimum value: ' + str(PORT_MIN))
 
-	if len(port_range) == 1:
-		port_range.append(port_range[0])
+    if len(port_range) == 1:
+        port_range.append(port_range[0])
 
-	elif port_range[0] > port_range[1]:
-		report_error('First port number cannot be higher than the second port number')
+    elif port_range[0] > port_range[1]:
+        report_error('First port number cannot be higher than the second port number')
 
-	return port_range
+    return port_range
 
 
 def check_port(host, portnr):
-	result = -1
+    result = -1
 
-	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-		# If the port is open, the return value is 0
-		result = s.connect_ex((host, portnr))
+        # If the port is open, the return value is 0
+        result = s.connect_ex((host, portnr))
 
-		s.close()
+        s.close()
 
-	except Exception as e:
-		pass
+    except Exception as e:
+        pass
 
-	return result == 0
+    return result == 0
 
 
-def check_target(host, min = 0, max = port_max, all_ports = False):
-	report('Checking target ' + host)
+def check_target(host, min=PORT_MIN, max=PORT_MAX, all_ports=False):
+    report('Checking target ' + host)
 
-	found_open_port = False
+    found_open_port = False
 
-	try:
-		for portnr in range(min, max + 1):
-			if check_port(host, portnr, True):
-				report(' !! Found open TCP port: ' + str(portnr))
-				found_open_port = True
+    try:
+        for portnr in range(min, max + 1):
+            if check_port(host, portnr):
+                report(' !! Found open TCP port: ' + str(portnr))
+                found_open_port = True
 
-	except KeyboardInterrupt as e:
-		pass
+    except KeyboardInterrupt as e:
+        pass
 
-	if not found_open_port:
-		print ('No open TCP ports found!')
+    if not found_open_port:
+        print('No open TCP ports found!')
 
 
 if __name__ == '__main__':
-	init()
+    init()
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 ***Port Scanner*** is a simple tool to help you find open TCP ports associated with a certain host.
 
 ## 1. Requirements
-* Python 3
+* Python 3 (tested on 3.6)
 * Command line or terminal
 
 ## 2. Usage
